b0a9cfcffc3cedfe1f4573dd317525d09d694892
RuanLMBS <ulle-2003@hotmail.com>
2026-06-03 19:48:13 -0300

    feat: added table data loading method

--- database/queries.py
+++ database/queries.py
@@ -1,14 +1,29 @@
 from sqlalchemy import inspect
+import pandas as pd
 from database.connection import get_engine
+
+def get_tables():
+    engine = get_engine()
+
+    inspector = inspect(engine)
+
+    return inspector.get_table_names()
 
 def search_columns(table):
     engine = get_engine()
 
-    insp = inspect(engine)
+    inspector = inspect(engine)
 
-    columns_info = insp.get_columns(table)
+    columns_info = inspector.get_columns(table)
 
     return [column["name"] for column in columns_info]
+
+def get_table_data(table_name):
+    engine = get_engine()
+
+    query = f"SELECT * FROM {table_name}"
+
+    return pd.read_sql(query, engine)
 
 def insert_data(df, table_name):
     engine = get_engine()
