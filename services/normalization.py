7e429af7b2acff1da9cb610b4128a65f483c3667
RuanLMBS <ulle-2003@hotmail.com>
2026-06-03 19:47:12 -0300

    feat: added region mapping to normalize region column in both csv and table insert

--- services/normalization.py
+++ services/normalization.py
@@ -1,8 +1,8 @@
 import pandas as pd
+from utils.regions import REGIOES
 
 def normalize_csv(file, delimiter, trim_spaces=True, convert_decimal=True):
     raw_file = pd.read_csv(file, sep=delimiter, encoding="latin1")
-
 
     if trim_spaces:
         raw_file.columns = raw_file.columns.str.strip()
@@ -13,4 +13,10 @@ def normalize_csv(file, delimiter, trim_spaces=True, convert_decimal=True):
          for column in raw_file.select_dtypes(include=["object", "string"]).columns:
              raw_file[column] = raw_file[column].str.replace(",",".", regex=False)
 
-    return raw_file
+    nrormalized_file = add_region_column(raw_file)
+    return nrormalized_file
+
+def add_region_column(file):
+    file["regiao"] = file["UF [-]"].map(REGIOES)
+
+    return file
