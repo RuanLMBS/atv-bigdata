ce4b5cc2111f7b385a10a53406324b10f2cd6058
RuanLMBS <ulle-2003@hotmail.com>
2026-06-07 11:02:53 -0300

    feat: adding null validator and report to normalization service

--- services/normalization.py
+++ services/normalization.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from utils.regions import REGIOES
 
-def normalize_csv(file, delimiter, trim_spaces=True, convert_decimal=True):
+def normalize_csv(file, delimiter, trim_spaces, convert_decimal):
     raw_file = pd.read_csv(file, sep=delimiter, encoding="latin1")
 
     if trim_spaces:
@@ -13,8 +13,16 @@ def normalize_csv(file, delimiter, trim_spaces=True, convert_decimal=True):
          for column in raw_file.select_dtypes(include=["object", "string"]).columns:
              raw_file[column] = raw_file[column].str.replace(",",".", regex=False)
 
-    nrormalized_file = add_region_column(raw_file)
-    return nrormalized_file
+    normalized_file = add_region_column(raw_file)
+
+    normalized_file = normalized_file.fillna(0)
+
+    null_report = normalized_file.isnull().sum()
+    
+    return normalized_file, null_report
+
+def validate_null (file):
+    return file.isnull().sum()
 
 def add_region_column(file):
     file["regiao"] = file["UF [-]"].map(REGIOES)
