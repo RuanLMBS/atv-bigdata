8e07cbf494d01e1fdcc8cb2c2fd40bf3345345b3
RuanLMBS <ulle-2003@hotmail.com>
2026-06-07 11:01:32 -0300

    feat: adding null values validator df, fixing parameter to method in service

--- pages/upload_page.py
+++ pages/upload_page.py
@@ -14,17 +14,24 @@ if uploaded_file:
     [";",","]
     )
 
-    decimal_separator = sl.radio("Qual é o separador dos campos decimais?", [",", "."])
-
     convert_decimal = sl.checkbox("Converter de vírgula para ponto (Campos Decimais)", value=True)
 
     trim_spaces = sl.checkbox("Remover espaços extras", value=True)
 
     if sl.button("Processar"):
-        normalized_file = normalize_csv(uploaded_file, delimiter, trim_spaces=trim_spaces, convert_decimal=convert_decimal)
+        normalized_file,null_report = normalize_csv(uploaded_file, delimiter, trim_spaces=trim_spaces, convert_decimal=convert_decimal)
 
-        sl.session_state["normalized_file"] = normalized_file
-
+        sl.session_state["normalized_file"] = normalized_file 
+        
         sl.success("Arquivo processado com sucesso!")
+
+        sl.subheader("Validação de Valores Nulos")
+
+        sl.dataframe(
+            null_report.reset_index().rename(
+                columns={ "index": "Coluna", 0: "Qtd. Nulos"}
+            )
+        )
+
         sl.write("Breve visualização dos dados:")
         sl.dataframe(normalized_file.head())
